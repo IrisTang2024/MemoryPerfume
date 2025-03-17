9cb7e8d8ff197a56515c22adc1fb6a585c1781e5
Iris Tang <iristang2023@gmail.com>
2025-03-17 17:11:57 -0500

    Add explicit route handlers and fix navigation links

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname)));
 
+// Explicit route handlers for static HTML pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
 // Simple server status endpoint
 app.get('/api/status', (req, res) => {
   res.json({ 
